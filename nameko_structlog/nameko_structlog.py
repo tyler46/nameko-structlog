f3257b0602cbdf8a9b0752733a094ae1e497fb87
Nikos Galanakis <galanakisn@gmail.com>
2020-09-27 14:30:19 +0300

    (add) configurable log keys
    (fix) removed indent of logs
    (add) uui at logs
    (fix) persist initial_values of BoundLogger in every call of structlog dependency
    (update) README file with new configuration example
    (fix) pytest config
    (fix) consistency on quotes

--- nameko_structlog/nameko_structlog.py
+++ nameko_structlog/nameko_structlog.py
@@ -1,6 +1,8 @@
 """
 Nameko Structlog Dependency Provider.
 """
+import uuid
+
 from nameko.extensions import DependencyProvider
 
 import structlog
@@ -17,11 +19,16 @@ class StructlogDependency(DependencyProvider):
     """Dependency Provider of Structlog."""
 
     def setup(self):
-        structlog_config = self.container.config.get('STRUCTLOG', {})
-        self.development_mode = structlog_config.get('DEVELOPMENT_MODE', False)
-        self.include_worker_name = structlog_config.get('WORKER_NAME', False)
-        self.unittesting = structlog_config.get('FOR_TESTING', False)
+        structlog_config = self.container.config.get("STRUCTLOG", {})
+        self.development_mode = structlog_config.get("DEVELOPMENT_MODE", False)
+        self.include_worker_name = structlog_config.get("WORKER_NAME", False)
+        self.unittesting = structlog_config.get("FOR_TESTING", False)
+        self.custom_keys = structlog_config.get("CUSTOM_KEYS", {})
+        self.sort_keys = structlog_config.get("SORT_KEYS", True)
         self.logger_by_service_name = dict()
+
+    def generate_uuid(self):
+        return str(uuid.uuid4())
 
     def get_dependency(self, worker_ctx):
         service_name = worker_ctx.service_name
@@ -29,9 +36,7 @@ class StructlogDependency(DependencyProvider):
 
             if self.unittesting:
                 log_factory = structlog.ReturnLoggerFactory
-                chain = [
-                    structlog.dev.ConsoleRenderer(colors=False)
-                ]
+                chain = [structlog.dev.ConsoleRenderer(colors=False)]
             else:
                 log_factory = structlog.stdlib.LoggerFactory
 
@@ -40,19 +45,15 @@ class StructlogDependency(DependencyProvider):
                     structlog.stdlib.add_logger_name,
                     structlog.stdlib.add_log_level,
                     structlog.stdlib.PositionalArgumentsFormatter(),
-                    structlog.processors.TimeStamper(fmt='iso'),
+                    structlog.processors.TimeStamper(fmt="iso"),
                     structlog.processors.StackInfoRenderer(),
                     structlog.processors.format_exc_info,
                     structlog.processors.UnicodeDecoder(),
                 ]
                 if self.development_mode:
-                    chain.append(
-                        structlog.dev.ConsoleRenderer(colors=use_colors)
-                    )
+                    chain.append(structlog.dev.ConsoleRenderer(colors=use_colors))
                 else:
-                    chain.append(
-                        structlog.processors.JSONRenderer(indent=2, sort_keys=True)
-                    )
+                    chain.extend([structlog.processors.JSONRenderer(sort_keys=self.sort_keys)])
 
             structlog.configure(
                 processors=chain,
@@ -62,9 +63,11 @@ class StructlogDependency(DependencyProvider):
                 cache_logger_on_first_use=True,
             )
 
-            if self.include_worker_name:
-                self.logger_by_service_name[service_name] = structlog.get_logger(service_name).new(entrypoint=worker_ctx.call_id)  # noqa pylint: disable=line-too-long
-            else:
-                self.logger_by_service_name[service_name] = structlog.get_logger(service_name).new()  # noqa pylint: disable=line-too-long
+            self.logger_by_service_name[service_name] = structlog.get_logger(service_name).new()
 
-        return self.logger_by_service_name[service_name]
+        initial_values = {"log_transaction_id": self.generate_uuid(), **self.custom_keys}
+
+        if self.include_worker_name:
+            initial_values.update({"entrypoint": worker_ctx.call_id})
+
+        return self.logger_by_service_name[service_name].bind(**initial_values)
